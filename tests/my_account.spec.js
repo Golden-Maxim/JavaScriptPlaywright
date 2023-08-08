ad2152b3211cab12bf4da537e99c446654e13992
Stovbchatyi <maksym.stovbchatyi@avenga.com>
2023-08-08 17:58:02 +0300

    added dotenv file

--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -1,15 +1,20 @@
 import { test } from "@playwright/test";
 import { MyAccountPage } from "../page-objects/MyAccountPage";
 import { getLoginToken } from "../api-calls/getLoginToken.js";
+import { adminDetails } from "../data/userDetails.js";
 
 test.only("My account login with cookies injection", async({page}) => {
 
     //Make a request to get login token 
-    const loginToken = await getLoginToken();
+    const loginToken = await getLoginToken(adminDetails.username, adminDetails.password);
 
-    console.warn({loginToken});
     //Inject login token into browser 
     const myAccountPage = new MyAccountPage(page);
     await myAccountPage.visit();
+    await page.evaluate(([loginTokenInsideBrowserCode]) =>{
+        document.cookie = "token=" + loginTokenInsideBrowserCode;
+    },[loginToken])
 
+    await myAccountPage.visit();
+    await myAccountPage.waitForPageHeading();
 })
